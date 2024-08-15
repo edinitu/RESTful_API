01dae6d7c2b943ea981c946df8b4a8e0f68d7650
edinitu <eduard.nitu@stud.etti.upb.ro>
2024-08-15 22:24:27 +0300

    redirect output to specific files
    Signed-off-by: edinitu <eduard.nitu@stud.etti.upb.ro>

--- run.sh
+++ run.sh
@@ -13,12 +13,12 @@ for ((i = 0 ; i < NUMBER_OF_INSTANCES ; i++)); do
   API_PORT=$(($BALANCER_PORT + $i + 1))
   INSTANCE=$(($i+1))
   echo "Will start an api instance on port ${API_PORT}"
-  ./server -port=${API_PORT} -no_of_instances=$NUMBER_OF_INSTANCES >> logs_instance_${INSTANCE}.txt &
+  ./server -port=${API_PORT} -no_of_instances=$NUMBER_OF_INSTANCES &> logs_instance_${INSTANCE}.txt &
 done
 
 cd ..
 cd balancer
 go build .
 echo "Will start load balancer on port ${BALANCER_PORT}"
-./balancer -number_of_instances=$NUMBER_OF_INSTANCES >> logs_load_balancer.txt
+./balancer -number_of_instances=$NUMBER_OF_INSTANCES &> logs_load_balancer.txt
 
