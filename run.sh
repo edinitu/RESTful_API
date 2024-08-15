efab44d015b33b31d02c18e86670ff56d0924dd9
edinitu <eduard.nitu@stud.etti.upb.ro>
2024-08-15 23:33:44 +0300

    improve run.sh
    Signed-off-by: edinitu <eduard.nitu@stud.etti.upb.ro>

--- run.sh
+++ run.sh
@@ -9,16 +9,24 @@ rm balancer/logs_load_balancer.txt
 cd server
 go build .
 
+pids=()
+
 for ((i = 0 ; i < NUMBER_OF_INSTANCES ; i++)); do
   API_PORT=$(($BALANCER_PORT + $i + 1))
   INSTANCE=$(($i+1))
   echo "Will start an api instance on port ${API_PORT}"
-  ./server -port=${API_PORT} -no_of_instances=$NUMBER_OF_INSTANCES &> logs_instance_${INSTANCE}.txt &
+  ./server -port=${API_PORT} -no_of_instances=$NUMBER_OF_INSTANCES &>> logs_instance_${INSTANCE}.txt &
+  pids+=($!)
 done
 
 cd ..
 cd balancer
 go build .
 echo "Will start load balancer on port ${BALANCER_PORT}"
-./balancer -number_of_instances=$NUMBER_OF_INSTANCES &> logs_load_balancer.txt
+./balancer -number_of_instances=$NUMBER_OF_INSTANCES &>> logs_load_balancer.txt & pids+=($!)
+
+for pid in "${pids[@]}"; do
+    wait $pid
+done
+
 
